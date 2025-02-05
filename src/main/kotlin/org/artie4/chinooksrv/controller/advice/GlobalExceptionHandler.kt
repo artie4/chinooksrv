689534309cbc89fa930897db55cafc7f8a89831e
arthur <gaartur4@gmail.com>
2025-02-05 22:43:16 +0300

    fix route, disable advice for now

--- src/main/kotlin/org/artie4/chinooksrv/controller/advice/GlobalExceptionHandler.kt
+++ src/main/kotlin/org/artie4/chinooksrv/controller/advice/GlobalExceptionHandler.kt
@@ -1,17 +1,19 @@
 package org.artie4.chinooksrv.controller.advice
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import jakarta.validation.ConstraintViolationException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.NoHandlerFoundException
 import java.time.LocalDateTime
 
-@RestControllerAdvice
+private val logger = KotlinLogging.logger { }
+
+// @RestControllerAdvice // todo break swagger
 class GlobalExceptionHandler {
     // Handle validation errors (e.g., @Valid fails)
     @ExceptionHandler(MethodArgumentNotValidException::class)
@@ -73,6 +75,8 @@ class GlobalExceptionHandler {
         ex: Exception,
         request: WebRequest,
     ): ResponseEntity<ErrorResponse> {
+        logger.error { "Error message: ${ex.message}, cause: ${ex.cause}" }
+
         val errorResponse =
             ErrorResponse(
                 status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
